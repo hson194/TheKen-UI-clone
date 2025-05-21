b882604f52c11e9ee0f9bc30ba75f39c121e13ba
hson194 <danghoaisontb@gmail.com>
2025-05-21 20:20:41 +0700

    feat: predefined Header and Footer to layout page

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
 function MainLayout(props: { children: React.ReactNode }) {
-  return <div>{props.children}</div>;
+  return (
+    <>
+      <Header />
+      <div>{props.children}</div>
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
