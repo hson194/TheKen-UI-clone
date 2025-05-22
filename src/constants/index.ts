1d2362dbef195b9960093a33137fe6d755fd59e8
hson194 <danghoaisontb@gmail.com>
2025-05-22 07:43:21 +0700

    feat: init header layout

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const PATH = {
+  HOME: "/",
+  ABOUT: "/about",
+  CART: "/cart",
+  DEMO: "/demo",
+  BLOG: "/blog",
+  ELEMENT: "/element",
+  PORTFOLIO: "/portfolio",
+  SHOP: "/shop",
+};
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,59 @@
+import { Link } from "react-router";
+import logo from "../assets/orange-logo-transparent-bg.png";
+import { PATH } from "../constants";
+import CartIcon from "../assets/icons/cartIcon";
+import SearchIcon from "../assets/icons/searchIcon";
+import BarsIcon from "../assets/icons/barsIcon";
+
 function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl">My Application</h1>
+    <header className="max-w-screen-xl w-full px-5 mx-auto bg-white flex justify-between">
+      <div>
+        <div className="xl:hidden flex items-center mr-2.5">
+          <BarsIcon />
+        </div>
+
+        <div className="logo my-[30px] ml-[20px] xl:ml-0">
+          <Link to={{ pathname: PATH.HOME }}>
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="menu w-full items-center hidden xl:flex">
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.HOME }}>Home</Link>
+          </div>
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.DEMO }}>Demo</Link>
+          </div>
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.ABOUT }}>About us</Link>
+          </div>
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.BLOG }}>Blog</Link>
+          </div>
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.PORTFOLIO }}>Portfolio</Link>
+          </div>
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.ELEMENT }}>Element</Link>
+          </div>
+          <div className="px-[20px]">
+            <Link to={{ pathname: PATH.SHOP }}>Shop</Link>
+          </div>
+        </div>
+        <div className="search-cart flex items-center">
+          <div>
+            <SearchIcon />
+          </div>
+          <div className="pl-[20px]">
+            <Link to={{ pathname: PATH.CART }}>
+              <CartIcon />
+            </Link>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
